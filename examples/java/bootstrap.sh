b0d7b7c5ffbe965e6a9fb45f0cd54d1c5c133852
Cody Lee White <cody.white0@walmart.com>
2017-08-17 10:10:55 -0500

    added yum proxy settings

--- examples/java/bootstrap.sh
+++ examples/java/bootstrap.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 echo "setting up proxy for user Vagrant"
-grep "http_proxy" /home/vagrant/.bash_profile
+grep "http_proxy" /home/vagrant/.bashrc
 if [ $? -eq 1 ]
     then
     sudo printf "export http_proxy=http://proxy.wal-mart.com:9080 \nexport https_proxy=http://proxy.wal-mart.com:9080 \n" >> /home/vagrant/.bashrc
     sudo printf "export no_proxy=.local,.wal-mart.com,.wamnetNAD,.walmart.com,.wmlink,.walmartlabs.com\n" >> /home/vagrant/.bashrc
-    #echo "setting yum to use proxy"
-    #sudo printf "poxy=http://proxy.wal-mart.com:9080\n" >> /etc/yum.conf
+    echo "setting yum to use proxy"
+    echo "removing default repos from yum"
+    sudo rm -rf /etc/yum.repos.d/*
+    echo "adding walmart repo to yum"
+    sudo printf "[myrepo] \nname=walmartrepo \nbaseurl=http://repos.walmart.com/base/centos/6.8 \n" > /etc/yum.repos.d/Centos-Base.repo
 fi
 
 echo "setting url for internal gem repo"
@@ -22,5 +25,6 @@ sudo /opt/chef/embedded/bin/gem install i18n -v 0.6.9 --conservative
 sudo /opt/chef/embedded/bin/gem install activesupport -v 3.2.11 --conservative
 echo "Done"
 
-#echo "install zip and unzip"
-#sudo yum install zip unzip -y
+echo "install zip and unzip"
+sudo yum clean metadata
+sudo yum install zip unzip -y
